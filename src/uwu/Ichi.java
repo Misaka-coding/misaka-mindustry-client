1750124ea9a0ce70c54395ea632f410ebed591dc
_owler_#0954 <63801085+owler0954@users.noreply.github.com>
2021-02-01 13:08:34 +0300

    с полями для текста

--- src/uwu/Ichi.java
+++ src/uwu/Ichi.java
@@ -10,10 +10,36 @@ import mindustry.mod.*;
 import mindustry.ui.dialogs.*;
 
 public class Ichi extends Mod{
+    private static String time = "60";
+    private static String reason = "60";
 
     public Ichi(){
         Events.on(ClientLoadEvent.class, e -> {
             Vars.ui.listfrag = new CustomPlayerListFragment();
         });
+    }
+
+    public static void showTempBanDialog(Player player){
+        time="60";
+        reason="R U L E S";
+        BaseDialog dialog=new BaseDialog("BAN");
+        dialog.addCloseButton();
+        dialog.add(player.name).row();
+        dialog.add(player.con.address);
+        dialog.field(timeGetter(),text->timeSetter(text));
+        dialog.field(reasonGetter(),text->reasonSetter(text)).addInputDialog().get();
+        dialog.button("@ok",()->Call.sendChatMessage("/ban "+time +" "+ player.con().address+ " "+reason));
+    }
+    public static void timeSetter(String s){
+        time=s;
+    }
+    public static String timeGetter(){
+        return time;
+    }
+    public static void reasonSetter(String s){
+        reason=s;
+    }
+    public static String reasonGetter(){
+        return reason;
     }
 }
