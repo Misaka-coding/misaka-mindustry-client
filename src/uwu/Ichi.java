66c86940268d1a8a5e71cfc85723036c6cf088a1
_owler_#0954 <63801085+owler0954@users.noreply.github.com>
2021-02-06 12:49:25 +0300

    re:re:re:method

--- src/uwu/Ichi.java
+++ src/uwu/Ichi.java
@@ -27,7 +27,7 @@ public class Ichi extends Mod {
         for (int x = 0; x < Vars.world.width(); x++) {
             for (int y = 0; y < Vars.world.height(); y++) {
                 Tile t = Vars.world.tile(x, y);
-                if (isConveyor(t.block())) {
+                if (t.block()!=null&&isConveyor(t.block())) {
                     Log.info(x+" "+y);
                     int tgt = t.build.rotation;
                     int lineChecker= 0;
