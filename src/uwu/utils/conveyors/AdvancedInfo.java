157655bffca93ffa283ab0323c15c836ad6a4b95
_owler_#0954 <63801085+owler0954@users.noreply.github.com>
2021-02-08 21:51:59 +0300

    вут

--- src/uwu/utils/conveyors/AdvancedInfo.java
+++ src/uwu/utils/conveyors/AdvancedInfo.java
@@ -9,7 +9,7 @@ public class AdvancedInfo {
     public AdvancedInfo(int x, int y) {
         BaseDialog d = new BaseDialog("Conveyor info");
         d.row();
-        d.center().add("Info about conveyor at " + x + y).row();
+        d.center().add("Info about conveyor at " + x + " " + y).row();
         d.center().add("Type: " + Vars.world.tile(x, y).block().name).row();
         //ToDO обавить интеграцию с историей
         d.center().button("Show history log", () -> {
@@ -22,6 +22,8 @@ public class AdvancedInfo {
             }
             hentai.trns(Vars.player.unit().angleTo(x * 8f, y * 8f), Vars.player.unit().type().speed);
             Vars.player.unit().moveAt(hentai);
+            Log.info(hentai.x + ' ' + hentai.y);
+            d.hide();
         }).size(250, 50).row();
         d.button("Close", () -> d.hide());
         d.closeOnBack();
